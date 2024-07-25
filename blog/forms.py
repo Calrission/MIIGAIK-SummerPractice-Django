4e49e99e1e862b14227d65cb0430c003e5e4f655
Artemii Strukov <kavrese@gmail.com>
2024-07-25 15:04:17 +0300

    init zip project

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,20 @@
-from django.forms import ModelForm
+from django import forms
+from .models import Comment
 
-from blog.models import Post
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False,
+                               widget=forms.Textarea)
 
 
-class PostForm(ModelForm):
-
+class CommentForm(forms.ModelForm):
     class Meta:
-        model = Post
-        fields = ['title', 'slug', 'body']
+        model = Comment
+        fields = ['name', 'email', 'body']
 
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
