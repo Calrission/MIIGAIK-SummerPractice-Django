9984bdf884d750404d46fbc94b6e41d85ee68c30
Artemii Strukov <kavrese@gmail.com>
2024-07-26 10:44:11 +0300

    refactored and fixed

--- account_site/forms.py
+++ account_site/forms.py
@@ -48,14 +48,6 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'email']
 
-    def clean_email(self):
-        data = self.cleaned_data['email']
-        qs = User.objects.exclude(id=self.instance.id) \
-            .filter(email=data)
-        if qs.exists():
-            raise forms.ValidationError('Email already in use.')
-        return data
-
 
 class ProfileEditForm(forms.ModelForm):
     photo = forms.FileField(required=False)
